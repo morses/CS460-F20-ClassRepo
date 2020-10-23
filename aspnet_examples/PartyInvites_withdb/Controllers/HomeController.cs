9e87757ae6de1ca42ba13fe7ea246e8134c9cae7
Scot Morse <morses@wou.edu>
2020-10-23 14:50:25 -0700

    initial commit of partyinvites_withdb example; just a straightforward copy of the previous example; no db added yet

---
+++ aspnet_examples/PartyInvites_withdb/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PartyInvites.Models;
+using System.Linq;
+
+namespace PartyInvites.Controllers {
+    public class HomeController : Controller {
+        
+        // GET /Home/Index   GET /home/index
+        public IActionResult Index() {
+            return View();
+        }
+
+        // GET /Home/RsvpForm
+        [HttpGet]
+        public ViewResult RsvpForm() {
+            return View();
+        }
+
+        // POST /home/rsvpform
+        [HttpPost]
+        public ViewResult RsvpForm(GuestResponse guestResponse) {
+            if (ModelState.IsValid) {
+                Repository.AddResponse(guestResponse);
+                return View("Thanks", guestResponse);
+            } else {
+                return View();
+            }
+        }
+
+        // GET /home/listresponses
+        public ViewResult ListResponses() {
+            //using LINQ
+            return View(Repository.Responses.Where(r => r.WillAttend == true));
+        }
+    }
+}
---
+++ aspnet_examples/PartyInvites_withdb/Models/GuestResponse.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PartyInvites.Models {
+
+    public class GuestResponse {
+
+        [Required(ErrorMessage = "Please enter your name")]
+        [StringLength(50)]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Please enter your email address")]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Please enter your phone number")]
+        public string Phone { get; set; }
+
+        [Range(1, 2)]
+        public int Guests {get; set;}
+
+        [Required(ErrorMessage = "Please specify whether you'll attend")]
+        public bool? WillAttend { get; set; }
+    }
+}
