c31fc899cdaed655816ec6bf2f285d6c5682f436
Scot Morse <morses@wou.edu>
2020-11-07 18:13:29 -0800

    add finished AJAX examples: random numbers and a plot, earthquakes example using the USGS api

---
+++ ajax/AJAX_Example/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AJAX_Example.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
