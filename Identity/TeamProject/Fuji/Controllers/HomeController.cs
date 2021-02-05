93ec7b70e8e393620922f70335fe1538eb17d2a7
Scot Morse <morses@wou.edu>
2021-02-04 18:56:35 -0800

    initial add of the example from 461 on using Identity for individual user accounts; goes with the video series on YouTube

---
+++ Identity/TeamProject/Fuji/Controllers/HomeController.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Fuji.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Fuji.Data;
+
+namespace Fuji.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly FujiDbContext _fujiDbContext;
+
+        public HomeController(ILogger<HomeController> logger, UserManager<IdentityUser> userManager, FujiDbContext fujiContext)
+        {
+            _logger = logger;
+            _userManager = userManager;
+            _fujiDbContext = fujiContext;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            // Information straight from the Controller (does not need to go to the database)
+            bool isAdmin = User.IsInRole("Admin");
+            bool isAuthenticated = User.Identity.IsAuthenticated;
+            string name = User.Identity.Name;
+            string authType = User.Identity.AuthenticationType;
+            
+            // Information from Identity through the user manager
+            string id = _userManager.GetUserId(User);       // reportedly does not need to hit the db
+            IdentityUser user = await _userManager.GetUserAsync(User);  // does go to the db
+            string email = user?.Email ?? "no email";
+            string phone = user?.PhoneNumber ?? "no phone number";
+            ViewBag.Message = $"User {name} is authenticated? {isAuthenticated} using type {authType} and is an Admin? {isAdmin}.  ID from Identity is {id}, email is {email} and phone is {phone}";
+
+            FujiUser fu = null;
+            if(id != null)
+            {
+                fu = _fujiDbContext.FujiUsers.Where(u => u.AspnetIdentityId == id).FirstOrDefault();
+            }
+
+            var appleList = _fujiDbContext.Apples.ToList();
+            MainPageVM vm = new MainPageVM { TheIdentityUser = user, TheFujiUser = fu, Apples = appleList };
+            return View(vm);
+        }
+
+        [Authorize]
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Identity/identity_videos_outline.md
@@ -0,0 +1,35 @@
+# Using Identity for Authorization and User Accounts
+
+1. Set up project with Individual User Accounts &#9989;
+2. Walk-through files and folders of the scaffolded project &#9989;
+3. A look at the database Identity uses; and migrations &#9989;
+4. Our architecture when we use authentication &#9989;
+5. Use it: getting basic info for the logged in user &#9989;
+6. A tale of 2 databases &#9989;
+```
+# update auth database using the current migration (when moving to a new database)
+dotnet ef database update --context ApplicationDbContext
+```
+7. Use it: create a user upon registration with first and last names &#9989;
+```
+# Reverse engineer our models using the correct connection (don’t reverse engineer Identity tables!)
+dotnet ef dbcontext scaffold Name=FujiConnection Microsoft.EntityFrameworkCore.SqlServer --context FujiDbContext --context-dir Data --output-dir Models --verbose --force
+
+# Scaffold controller with views using the correct context
+dotnet aspnet-codegenerator controller -name FujiUsersController -m FujiUser -dc FujiDbContext --relativeFolderPath Controllers --useDefaultLayout --referenceScriptLibraries --force
+```
+8. Use it: per-user operations – let them eat apples &#9989;
+9. Some simple configuration changes
+10. Authorization policies
+    - per method
+    - per class
+    - per application
+    - "allow all except" or "deny all except" strategies
+11. Deploy it to Azure
+```
+# Generate sql script to create or update Identity db on Azure
+dotnet ef migrations script --context ApplicationDbContext --output Data\UpdateIdentityAzure.sql
+```
+12. How to seed users
+13. How to seed a super-admin role and account
+14. How Identity uses cookies to make this work
