a5ceeda461da13bb4bf1a94f24e83915a4c9a336
Scot Morse <morses@wou.edu>
2021-03-29 12:10:25 -0700

    initial commit of UnitTesting directory where we'll put examples about unit testing; has initial project to get things set up, with only minimal examples of actual testing

---
+++ UnitTesting/GetStarted/GetStarted/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using GetStarted.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GetStarted.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Capitalize(string input)
+        {
+            string capitalized = char.ToUpper(input[0]) + input.Substring(1);
+            return View("Capitalize",capitalized);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ UnitTesting/GetStarted/GetStarted/wwwroot/js/sum.js
@@ -0,0 +1,6 @@
+﻿function sum(a, b)
+{
+    return a + b;
+}
+
+console.log(sum(4, 5));
