ddd72de9d3a40dbb8fe05049986f1244faa39805
Scot Morse <morses@wou.edu>
2021-05-03 11:53:47 -0700

    adding the BDD test project to the Fuji example; currently has feature file and step definitions for the sample Calculator feature

---
+++ UnitTesting/RefactoringToRepository/Fuji.BDDTests/src/Calculator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fuji.BDDTests.src
+{
+    public class Calculator
+    {
+        public int FirstNumber { get; set; }
+        public int SecondNumber { get; set; }
+
+        public int Add()
+        {
+            //throw new NotImplementedException();
+            return FirstNumber + SecondNumber;
+        }
+    }
+}
